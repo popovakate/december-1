6b3aaf03ba7c5d3890f05a6d7fe44cdf69f9d1d7
popovakate <108681165+popovakate@users.noreply.github.com>
2022-12-02 11:42:12 +0200

    Started building an app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello Web</p>
-      </header>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project is coded by Kate Popova and is{" "}
+          <a href="https://github.com/popovakate/december-1">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
