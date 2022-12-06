47246656ac8d6d87febb46fdc3a55b7c5ebd8d38
popovakate <108681165+popovakate@users.noreply.github.com>
2022-12-06 18:01:08 +0200

    trying to fix

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -12,6 +12,9 @@ export default function FormattedDate(props) {
   ];
   let day = days[props.date.getDay()];
   let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = props.date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
