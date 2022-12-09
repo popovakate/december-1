f313bb80e710295baa1143944c47fa548b18344f
popovakate <108681165+popovakate@users.noreply.github.com>
2022-12-09 12:10:18 +0200

    built forecast

--- src/weatherInfo.js
+++ src/weatherInfo.js
@@ -16,11 +16,11 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="d-flex">
-            <div>
+            <div className="float-left">
               <WeatherIcon code={props.data.icon} size={52} />
             </div>
 
-            <div>
+            <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
